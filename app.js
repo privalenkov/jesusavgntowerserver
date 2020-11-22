878fc3473f047f841898508447e186f92471fcb8
Kirill Privalenkov <privalenkovkirill@gmail.com>
2020-11-22 18:22:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY || path.join(__dirname, 'ssl', 'privateKey.key')),
+  cert: fs.readFileSync(process.env.SSL_CERT || path.join(__dirname, 'ssl', 'certificate.crt')),
+};
+
+const server = require('https').createServer(httpsOptions, app);
+const io = require('socket.io')(server);
+
+server.listen(443);
+require('http').createServer(app).listen(80);
+
+app.use(function (req, res, next) {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect('https://' + req.hostname + req.url);
+  }
+});
+
+app.use(express.static(__dirname + '/../client'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: '../client' });
+});
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+function saveToFile() {
+  fs.writeFileSync('../hesusappdata', String(clickCount.toFixed(3)));
+}
+
+function loadFromFile() {
+  try {
+    const res = fs.readFileSync('../hesusappdata').toString();
+    return parseInt(res);
+  } catch {
+    return 50.000;
+  }
+}
+
+function throttle(func, ms = 100) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+let clickCount = loadFromFile();
+
+const updateClick = throttle((socket) => {
+  socket.broadcast.emit('clickInc', clickCount);
+}, 200);
+
+const updateUserCount = throttle(() => {
+  io.sockets.emit('updateUserCount', io.engine.clientsCount);
+}, 500);
+
+io.on('connection', (socket) => {
+  const clickInc = throttle((count) => {
+    clickCount += 5;
+    updateClick(socket);
+  }, 50);
+
+  const clickDec = throttle((count) => {
+    if(clickCount <= 0) return;
+    clickCount -= 5;
+    updateClick(socket);
+  }, 50);
+
+  socket.on('clickInc', (count = 1) => {
+    if (count < 1 || count > 20) return;
+
+    clickInc(count);
+  });
+
+  socket.on('clickDec', (count = 1) => {
+    if (count < 1 || count > 20) return;
+
+    clickDec(count);
+  });
+
+  socket.on('disconnect', updateUserCount);
+
+  socket.emit('connection', clickCount);
+  updateUserCount();
+});
+
+setInterval(saveToFile, 5000);
