b8af0b556a0269f589146b76948846bf6628d959
Kirill Privalenkov <privalenkovkirill@gmail.com>
2020-11-25 00:21:47 +0300

    autodec

--- app.js
+++ app.js
@@ -79,29 +79,42 @@ const updateClick = throttle((socket) => {
   socket.broadcast.emit('clickInc', clickCount);
 }, 200);
 
+const updateClickAuto = throttle(() => {
+  io.sockets.emit('autoDec', clickCount);
+}, 200);
+
 const updateUserCount = throttle(() => {
   io.sockets.emit('updateUserCount', io.engine.clientsCount);
 }, 500);
-
+let autoDecBool = false
+let counter = 0
 io.on('connection', (socket) => {
+  
+
+
   const clickInc = throttle((count) => {
     clickCount += 0.001;
     updateClick(socket);
+    autoDecBool = false
   }, 50);
 
   const clickDec = throttle((count) => {
-    if(clickCount <= 0) return;
-    clickCount -= 0.001;
+    if(clickCount <= 0) return
+    clickCount -= 0.003;
+    if(clickCount < 0) clickCount = 0
     updateClick(socket);
   }, 50);
 
   socket.on('clickInc', (count = 1) => {
+    autoDecBool = true
+    counter = 0
     if (count < 1 || count > 20) return;
 
     clickInc(count);
   });
 
   socket.on('clickDec', (count = 1) => {
+    
     if (count < 1 || count > 20) return;
 
     clickDec(count);
@@ -112,5 +125,22 @@ io.on('connection', (socket) => {
   socket.emit('connection', clickCount);
   updateUserCount();
 });
+
+setInterval(() => {
+  console.log(counter)
+  if(clickCount > 0) {
+    if(!autoDecBool && counter <= 360) {
+      counter++
+    }
+    if(counter >= 360) { 
+      if(clickCount <= 0) return
+      clickCount -= 0.01;
+      updateClickAuto()
+      if(clickCount < 0) clickCount = 0
+    }
+  }
+}, 5000);
+
+
 
 setInterval(saveToFile, 5000);
